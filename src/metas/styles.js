9db29ec1490167416962692ab6da0e478af3975a
allandev7 <allansilva@MacBook-Pro-de-Allan.local>
2020-04-02 00:52:07 -0300

    Teste para consertar config git

--- src/metas/styles.js
+++ src/metas/styles.js
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
 import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
-
 const styles = StyleSheet.create({
     all: {
         height: '100%',
